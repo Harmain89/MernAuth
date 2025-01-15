0d298a32a91025efa9d38d90798a9a14b591958b
Harmain Rizwan <harmainrizwan98@outlook.com>
2025-01-15 20:31:22 +0500

    work done once

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json({limit: "16kb"}))
+// app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(cookieParser())
+app.use(cors({credentials: true}))
+
+app.get('/', (req, res) => {
+    res.json({message: "API is working here...."})
+})
+
+connectDB().then(() => {
+    
+    app.listen(port, () => {
+        console.log(`Server is runnning at port no ${port}`);
+    })
+
+    app.on("error", (error) => {
+        console.log("ERRR: ", error);
+        throw error;
+    })
+
+})
+.catch((err) => {
+    console.log("Mongo db connection failed: ", err);
+})
